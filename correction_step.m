f39388d2fa249b88f1bbe063b2ded10f6d978ca1
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-11-20 12:33:22 +0500

    Update correction_step.m

--- correction_step.m
+++ correction_step.m
@@ -4,3 +4,4 @@ function [xhat_k,P_k] = correction_step(S_k,yhat_predict,...
 K_k = (P_predict* H')*inv(S_k); 
 xhat_k = xhat_predict + K_k*(y_k - yhat_predict); 
 P_k = P_predict - K_k*H*P_predict; 
+end
